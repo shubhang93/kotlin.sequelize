7499c1570cbd0381a572a05b83243436408c9321
shubhangb <shubhang@channelbridge.in>
2019-03-16 14:21:25 +0530

    fix: move DSL Makers to new modules

---
+++ src/main/kotlin/org/sequelize/entity/DSLMakers.kt
@@ -0,0 +1,46 @@
+package org.sequelize.entity
+
+@DslMarker
+annotation class EntityRelationMappingDSL
+
+private fun Map<String, Table>.toImmutableMap(): Map<String, Table> {
+    val entries = this.entries.toList()
+    val pairs = entries.map { Pair(it.key, it.value) }
+    return mapOf(*pairs.toTypedArray())
+}
+
+data class Table(
+    val tableName: String,
+    val primaryKey: String,
+    val childEntity: String?,
+    val foreignKey: String?,
+    val compoundPrimaryKeys: List<String>,
+    val nullableColumns: List<String>
+)
+
+@EntityRelationMappingDSL
+class TableBuilder {
+    var tableName = ""
+    var primaryKey = ""
+    var foreignKey: String? = null
+    var childEntity: String? = null
+    var compoundPrimaryKeys = listOf<String>()
+    var nullableColumns: List<String> = listOf()
+
+    fun build(): Table {
+        return Table(tableName, primaryKey, foreignKey, childEntity, compoundPrimaryKeys, nullableColumns)
+    }
+}
+
+@EntityRelationMappingDSL
+class EntityRelationMapping {
+    private val tableMapping = mutableMapOf<String, Table>()
+    fun table(block: TableBuilder.() -> Unit) {
+        val table = TableBuilder().apply(block).build()
+        tableMapping[table.tableName] = table
+    }
+
+    fun build(): Map<String, Table> {
+        return tableMapping.toImmutableMap()
+    }
+}
--- src/main/kotlin/org/sequelize/entity/EntityRelationMapping.kt
+++ src/main/kotlin/org/sequelize/entity/EntityRelationMapping.kt
@@ -1,57 +1,12 @@
 package org.sequelize.entity
 
-data class Table(
-    val tableName: String,
-    val primaryKey: String,
-    val childEntity: String?,
-    val foreignKey: String?,
-    val compoundPrimaryKeys: List<String>,
-    val nullableColumns: List<String>
-)
-
-
-@DslMarker
-annotation class EntityRelationMappingDSL
-
-
-fun Map<String, Table>.toImmutableMap(): Map<String, Table> {
-    val entries = this.entries.toList()
-    val pairs = entries.map { Pair(it.key, it.value) }
-    return mapOf(*pairs.toTypedArray())
-}
-
-@EntityRelationMappingDSL
-class EntityRelationMapping {
-    private val tableMapping = mutableMapOf<String, Table>()
-    fun table(block: TableBuilder.() -> Unit) {
-        val table = TableBuilder().apply(block).build()
-        tableMapping[table.tableName] = table
-    }
-
-    fun build(): Map<String, Table> {
-        return tableMapping.toImmutableMap()
-    }
-}
-
 
 fun entityRelationMapping(block: EntityRelationMapping.() -> Unit): Map<String, Table> {
     return EntityRelationMapping().apply(block).build()
 }
 
 
-@EntityRelationMappingDSL
-class TableBuilder {
-    var tableName = ""
-    var primaryKey = ""
-    var foreignKey: String? = null
-    var childEntity: String? = null
-    var compoundPrimaryKeys = listOf<String>()
-    var nullableColumns: List<String> = listOf()
 
-    fun build(): Table {
-        return Table(tableName, primaryKey, foreignKey, childEntity, compoundPrimaryKeys, nullableColumns)
-    }
-}
 
 
 
