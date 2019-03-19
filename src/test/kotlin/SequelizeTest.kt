4b32e5ec9be45ed20bcc757cf9ce174482a25035
shubhangb <shubhang@channelbridge.in>
2019-03-19 22:33:01 +0530

    feat: add log

--- src/test/kotlin/SequelizeTest.kt
+++ src/test/kotlin/SequelizeTest.kt
@@ -21,6 +21,7 @@ class SequelizeTest {
 
     @Test
     fun testQueryMapGeneration() {
+        println("Queries Path --> $PATH")
         val qm = extractQueryMap(PATH)
         Assert.assertTrue(qm.size > 1)
     }
